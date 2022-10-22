4fb78957ff9b5d5ab4807a0c0ffd77c26108ec1a
ALeks Baranov <aleks.feedback@gmail.com>
2022-10-22 16:20:44 +0300

    create select and inputs

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ let menu = document.querySelector('.menu__body');
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
+
+////////////////
+
+const selectSingle = document.querySelector('.form__select');
+const selectSingle_title = selectSingle.querySelector('.form__select-title');
+const selectSingle_labels = selectSingle.querySelectorAll('.form__select-label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
